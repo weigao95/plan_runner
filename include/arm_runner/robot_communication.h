6c2aa4b7976e2e620be59b73d9090f6b7578d977
wei <gaowei19951004@gmail.com>
2019-08-27 23:03:55 -0400

    update

--- include/arm_runner/robot_communication.h
+++ include/arm_runner/robot_communication.h
@@ -5,15 +5,37 @@
 #pragma once
 
 #include "arm_runner/communication_types.h"
+
+#include <vector>
+#include <functional>
 #include <boost/circular_buffer.hpp>
 
 namespace arm_runner {
 
     class RobotCommunication {
     public:
+        static constexpr int HISTORY_HORIZON = 100;
+        RobotCommunication();
+        virtual ~RobotCommunication() = default;
+
+        // The measure and command interface
+        void GetMeasurement(RobotArmMeasurement& measurement);
+        void SendCommand(const RobotArmCommand& command);
+
     protected:
+        // The actual interface that communicate with the robot
         virtual void getRawMeasurement(RobotArmMeasurement& measurement) = 0;
         virtual void sendRawCommand(const RobotArmCommand& command) = 0;
+
+        // The buffer hold the history measurement and command
+        boost::circular_buffer<RobotArmMeasurement> measurement_history_;
+        boost::circular_buffer<RobotArmCommand> command_history_;
+
+        // The processor for command and measurement
+        using CommandProcessor = std::function<void(RobotArmCommand&)>;
+        using MeasurementProcessor = std::function<void(RobotArmMeasurement&)>;
+        std::vector<CommandProcessor> command_processor_stack_;
+        std::vector<MeasurementProcessor> measurement_processor_stack_;
     };
 
 }
--- src/common/robot_communication.cpp
+++ src/common/robot_communication.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "arm_runner/robot_communication.h"
+
+arm_runner::RobotCommunication::RobotCommunication()
+: measurement_history_(RobotCommunication::HISTORY_HORIZON),
+  command_history_(RobotCommunication::HISTORY_HORIZON) {}
+
+void arm_runner::RobotCommunication::GetMeasurement(RobotArmMeasurement& measurement) {
+    // Get the raw measurement
+    getRawMeasurement(measurement);
+
+    // Do processing
+    for(const auto& processor : measurement_processor_stack_) {
+        processor(measurement);
+    }
+
+    // Save
+    measurement_history_.push_back(measurement);
+}
+
+void arm_runner::RobotCommunication::SendCommand(const RobotArmCommand & command_in) {
+    // Do processing
+    auto command = command_in;
+    for(const auto& processor : command_processor_stack_) {
+        processor(command);
+    }
+
+    // Save
+    command_history_.push_back(command);
+
+    // Handle to robot
+    sendRawCommand(command_history_.back());
+}
