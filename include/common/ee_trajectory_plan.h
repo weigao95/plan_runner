28c4359b00e829bec2e13a0f73b5dd3f5c97921d
wei <gaowei19951004@gmail.com>
2019-08-30 14:59:48 -0400

    init for ee traj plan

---
+++ include/common/ee_trajectory_plan.h
@@ -0,0 +1,21 @@
+//
+// Created by wei on 8/30/19.
+//
+
+#pragma once
+
+#include <memory>
+#include <drake/common/trajectories/piecewise_polynomial.h>
+#include <drake/multibody/rigid_body_tree.h>
+#include <trajectory_msgs/JointTrajectory.h>
+#include <trajectory_msgs/JointTrajectoryPoint.h>
+#include <ros/ros.h>
+
+#include "common/plan_base.h"
+
+
+namespace arm_runner {
+
+
+
+}
---
+++ src/common/ee_trajectory_plan.cpp
@@ -0,0 +1,4 @@
+//
+// Created by wei on 8/30/19.
+//
+
