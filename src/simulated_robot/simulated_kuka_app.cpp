050e22172d6d0c604044dbcb8777cc48479d8abc
wei <gaowei19951004@gmail.com>
2019-08-29 21:40:24 -0400

    need init on robot

--- src/simulated_robot/simulated_kuka_app.cpp
+++ src/simulated_robot/simulated_kuka_app.cpp
@@ -9,6 +9,7 @@
 #include <drake/multibody/rigid_body_tree.h>
 
 #include "common/plan_supervisor.h"
+#include "common/joint_publish_processor.h"
 #include "simulated_robot/common_robot_model.h"
 #include "simulated_robot/simulated_robot.h"
 
@@ -24,10 +25,15 @@ int main(int argc, char* argv[]) {
     using namespace arm_runner;
     std::unique_ptr<RobotCommunication> robot_arm = constructSimulatedKukaDefault(simulation_time);
 
-    // Empty init
+    // Empty init of node
     std::vector<std::pair<std::string, std::string>> vector_map;
     ros::init(vector_map, "plan_runner");
     ros::NodeHandle nh("plan_runner"); // sets the node's namespace
+
+    // The publisher
+    JointPublishProcessor::Ptr publisher = std::make_shared<JointPublishProcessor>(
+        nh, "/iiwa/joint_states", getKukaJointNameList());
+    robot_arm->AddMeasurementProcessor(publisher);
 
     // The rigid body tree
     auto tree = constructDefaultKukaRBT();
